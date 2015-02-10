c069217f9f13d28affcf6ddefb1bb5c3984751b8
Ricardo Quesada <ricardoquesada@gmail.com>
2015-02-10 15:50:50 -0800

    some fixes to make chipmunk and curl compile

--- cmake/Modules/FindCURL.cmake
+++ cmake/Modules/FindCURL.cmake
@@ -27,60 +27,28 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-set(CURL_LIBRARY_NAMES
-  curl
-# Windows MSVC prebuilts:
-  curllib
-  libcurl_imp
-  curllib_static
-# Windows older "Win32 - MSVC" prebuilts (libcurl.lib, e.g. libcurl-7.15.5-win32-msvc.zip):
-  libcurl
-  )
-
-find_package(PkgConfig)
-if(PKG_CONFIG_FOUND)
-  pkg_search_module(CURL QUIET libcurl)
-endif()
-
-if(NOT CURL_FOUND)
-
-    # Look for the header file.
-    find_path(CURL_INCLUDE_DIR NAMES curl/curl.h)
-    mark_as_advanced(CURL_INCLUDE_DIR)
-
-    # Look for the library (sorted from most current/relevant entry to least).
-    find_library(CURL_LIBRARY NAMES
-        curl
-      # Windows MSVC prebuilts:
-        curllib
-        libcurl_imp
-        curllib_static
-      # Windows older "Win32 - MSVC" prebuilts (libcurl.lib, e.g. libcurl-7.15.5-win32-msvc.zip):
-        libcurl
+find_path(CURL_INCLUDE_DIR curl.h
+    HINTS
+    ENV CURL_DIR
+    PATH_SUFFIXES include/curl include
     )
-    mark_as_advanced(CURL_LIBRARY)
 
-    if(CURL_INCLUDE_DIR)
-      foreach(_curl_version_header curlver.h curl.h)
-        if(EXISTS "${CURL_INCLUDE_DIR}/curl/${_curl_version_header}")
-          file(STRINGS "${CURL_INCLUDE_DIR}/curl/${_curl_version_header}" curl_version_str REGEX "^#define[\t ]+LIBCURL_VERSION[\t ]+\".*\"")
+set(CURL_NAMES ${CURL_NAMES} curl)
+find_library(CURL_LIBRARY
+    NAMES ${CURL_NAMES}
+    HINTS
+    ENV CURL_DIR
+    PATH_SUFFIXES lib
+    )
 
-          string(REGEX REPLACE "^#define[\t ]+LIBCURL_VERSION[\t ]+\"([^\"]*)\".*" "\\1" CURL_VERSION_STRING "${curl_version_str}")
-          unset(curl_version_str)
-          break()
-        endif()
-      endforeach()
-    endif()
+# handle the QUIETLY and REQUIRED arguments and set CURL_FOUND to TRUE if
+# all listed variables are TRUE
+include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL DEFAULT_MSG CURL_LIBRARY CURL_INCLUDE_DIR)
 
-    include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(CURL
-                                      REQUIRED_VARS CURL_LIBRARY CURL_INCLUDE_DIR
-                                      VERSION_VAR CURL_VERSION_STRING)
-
-    if(CURL_FOUND)
-      set(CURL_LIBRARIES ${CURL_LIBRARY})
-      set(CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR})
-    endif()
-
+if(CURL_FOUND)
+  set(CURL_INCLUDE_DIRS ${CURL_INCLUDE_DIR})
+  set(CURL_LIBRARIES ${CURL_LIBRARY})
 endif()
 
+mark_as_advanced(CURL_LIBRARY CURL_INCLUDE_DIRS )
