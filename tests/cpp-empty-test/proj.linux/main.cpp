e76037d1d60312a3702b6545441001084b61fda3
Ricardo Quesada <ricardoquesada@gmail.com>
2015-02-27 11:50:20 -0800

    using sysmips();

--- tests/cpp-empty-test/proj.linux/main.cpp
+++ tests/cpp-empty-test/proj.linux/main.cpp
@@ -6,10 +6,14 @@
 #include <unistd.h>
 #include <string>
 
+#include <sys/sysmips.h>
+
 USING_NS_CC;
 
 int main(int argc, char **argv)
 {
+    printf("main()\n");
+    sysmips(MIPS_FIXADE, 1);
     // create the application instance
     AppDelegate app;
     return Application::getInstance()->run();
