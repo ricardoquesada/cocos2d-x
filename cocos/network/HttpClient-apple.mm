e408fe0b9303813d20bab2f7bf548f3b7af416a6
Ricardo Quesada <ricardoquesada@gmail.com>
2015-07-29 13:59:46 -0700

    Adds more files to Xcode project
    ...even if they are not being compiled. Much easier to add missing
    functionality to the rest of the platforms

--- cocos/network/HttpClient-apple.mm
+++ cocos/network/HttpClient-apple.mm
@@ -24,6 +24,9 @@
  THE SOFTWARE.
  ****************************************************************************/
 
+#include "platform/CCPlatformConfig.h"
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+
 #include "HttpClient.h"
 
 #include <queue>
@@ -601,5 +604,8 @@ const std::string& HttpClient::getSSLVerification()
 }
 
 NS_CC_END
+
+#endif // #if CC_TARGET_PLATFORM == CC_PLATFORM_MAC
+
 
 
