00e0d7b07cd8aa77e8a07d70ed64682044e7ac14
Ricardo Quesada <ricardoquesada@gmail.com>
2015-09-04 15:36:08 -0700

    compiles both for 32 / 64 bit android

--- samples/Javascript/MoonWarriors/proj.android/jni/Application.mk
+++ samples/Javascript/MoonWarriors/proj.android/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -DCOCOS2D_JAVASCRIPT=1
 APP_CPPFLAGS += -DCOCOS2D_DEBUG=1 -DCC_ENABLE_CHIPMUNK_INTEGRATION=1
-#APP_ABI := arm64-v8a armeabi-v7a
+APP_ABI := arm64-v8a armeabi-v7a
 #APP_ABI := arm64-v8a
-APP_ABI := armeabi-v7a
+#APP_ABI := armeabi-v7a
